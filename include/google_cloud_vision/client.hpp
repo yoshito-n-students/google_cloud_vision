9f5d75db3d0ac6e741e71d91cbfd0d4b1c371342
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2017-05-02 11:58:23 +0900

    draft codes (never tested)

---
+++ include/google_cloud_vision/client.hpp
@@ -0,0 +1,69 @@
+#ifndef GOOGLE_CLOUD_VISION_CLIENT
+#define GOOGLE_CLOUD_VISION_CLIENT
+
+#include <sstream>
+#include <string>
+
+#include <boost/network/include/http/client.hpp>
+
+#include <google_cloud_vision/json_parser.hpp>
+#include <google_cloud_vision/namespace.hpp>
+#include <google_cloud_vision_srvs/Annotate.h>
+
+namespace google_cloud_vision {
+
+class Client {
+public:
+  Client(const std::string &key)
+      : http_destination_("https://vision.googleapis.com/v1/images:annotate?key=" + key) {}
+
+  virtual ~Client() {}
+
+  void call(srvs::Annotate &ros_service) { call(ros_service.request, ros_service.response); }
+
+  void call(const srvs::Annotate::Request &ros_request, srvs::Annotate::Response &ros_response) {
+    // layers (top to bottom):
+    //   ROS:  request and response are written in ROS message types
+    //   JSON: written in JSON texts
+    //   HTTP: written in HTTP messages
+
+    try {
+      // ROS -> JSON
+      std::ostringstream json_request;
+      writeJson(json_request, ros_request, false);
+
+      // JSON -> HTTP
+      bnh::client::request http_request;
+      bnh::destination(http_request, http_destination_);
+      bnh::body(http_request, json_request.str());
+
+      // call the google cloud vision apis on the HTTP layer
+      bnh::client::response http_response;
+      {
+        bnh::client::options http_options;
+        http_options.always_verify_peer(true).timeout(60); // TODO: user-specified timeout
+        bnh::client http_client(http_options);
+        http_response = http_client.post(http_request);
+      }
+
+      // HTTP -> JSON
+      std::istringstream json_response(bnh::body(http_response));
+
+      // JSON -> ROS
+      readJson(json_response, ros_response);
+    } catch (const std::exception &error) {
+      // set a local runtime error when happened
+      msgs::AnnotateImageResponse error_response;
+      error_response.has_error = true;
+      error_response.error.message = error.what();
+      ros_response = srvs::Annotate::Response();
+      ros_response.responses.resize(ros_request.requests.size(), error_response);
+    }
+  }
+
+private:
+  const std::string http_destination_;
+};
+}
+
+#endif
--- include/google_cloud_vision/namespace.hpp
+++ include/google_cloud_vision/namespace.hpp
@@ -2,7 +2,9 @@
 #define GOOGLE_CLOUD_VISION_NAMESPACE
 
 namespace boost {
-namespace asio {}
+namespace network {
+namespace http {}
+}
 namespace property_tree {}
 }
 
@@ -11,7 +13,7 @@ namespace google_cloud_vision_msgs {}
 namespace google_cloud_vision_srvs {}
 
 namespace google_cloud_vision {
-namespace ba = ::boost::asio;
+namespace bnh = ::boost::network::http;
 namespace bp = ::boost::property_tree;
 namespace msgs = ::google_cloud_vision_msgs;
 namespace srvs = ::google_cloud_vision_srvs;
